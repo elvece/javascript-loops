d5c6d604b50cc7f9bb950f74af586a3d51362130
elvece <lvcifferello@gmail.com>
2015-07-13 08:11:02 -0600

    first commit, solutions to first two problems

---
+++ loops.js
@@ -0,0 +1,17 @@
+//answering example problem with while insted//
+var number = 1;
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
+
+//solution 1//
+for (i = 0; i <= 1000; i+=100) {
+  console.log(i);
+}
+
+//solution 2//
+for (i = 1; i <= 128; i++) {
+  (exp(2, i));
+  console.log(i);
+}
