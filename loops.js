be2ff9c2026fd27d8ec8324d65165076e1b7e167
elvece <lvcifferello@gmail.com>
2015-07-14 00:10:21 -0600

    added solution 6

--- loops.js
+++ loops.js
@@ -1,17 +1,42 @@
-//answering example problem with while insted//
+//answering example problem with while insted
 var number = 1;
 while (number <= 10) {
     console.log(number);
     number++;
 }
 
-//solution 1//
+//solution 1
 for (i = 0; i <= 1000; i+=100) {
   console.log(i);
 }
 
-//solution 2//
-for (i = 1; i <= 128; i++) {
-  (exp(2, i));
+//solution 2
+for (i = 1; i <= 128; i*=2) {
   console.log(i);
+}
+
+//solution 3
+for (i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
+
+//solution 4
+for (i = 3; i <= 15; i+=2) {
+  console.log(i);
+}
+
+//solution 5
+for (i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+//solution 6 
+for (i = 0; i < 12; i++) {
+  console.log(Math.floor(i/3) + 1);
+}
+
+//solution 7
+for (i = 0; i <= 4; i++) {
+  var j =
+  console.log();
 }
